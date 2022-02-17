5987690b6bba7673b5ecaf1b1982275b23cfba15
swaroop_a.s <swaroopanand325@gmail.com>
2022-02-17 17:58:12 +0530

    added

---
+++ gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function (grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    //     pkg: grunt.file.readJSON("package.json"),
+    //     uglify: {
+    //       options: {
+    //         banner:
+    //           '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+    //       },
+    //       build: {
+    //         src: "src/<%= pkg.name %>.js",
+    //         dest: "build/<%= pkg.name %>.min.js",
+    //       },
+    //     },
+  });
+
+  //   // Load the plugin that provides the "uglify" task.
+  //   grunt.loadNpmTasks("grunt-contrib-uglify");
+
+  //Task with parameter.
+  grunt.registerTask("task1", "task1", function (name) {
+    console.log("hi " + name);
+  });
+
+  //Task without parameter.
+  grunt.registerTask("task2", "task2", function () {
+    console.log("basic task");
+  });
+
+  //default
+  grunt.registerTask("default", ["task1:Swaroop", "task2"]);
+};
